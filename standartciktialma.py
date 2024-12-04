133af7772179fd0dd701135960dd5d0a4a898d5e
Ebrar Kaya <ebrkaya9852@gmail.com>
2024-12-04 19:41:18 +0300

    Update standartciktialma.py

--- standartciktialma.py
+++ standartciktialma.py
@@ -6,3 +6,5 @@ print("İsminiz:",ad)
 print("Anne İsminiz:",anneAdi)
 print("Baba İsminiz:",babaAdi)
 print("Yasiniz:",yas)
+
+(https://github.com/ebrarrkaya/Mant-k/blob/d19373e4ec3dc910758db99f95117d73080a84f0/standart%C3%A7%C4%B1kt%C4%B1.png)
